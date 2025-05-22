9c06d6052e24f83d9a821e5f85535c9496fd627a
nrismawardah <nrismawardah@gmail.com>
2025-05-23 00:22:05 +0700

    Membuat container untuk search bar

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:plantapp/constants.dart';
 
 class Body extends StatelessWidget {
@@ -10,15 +12,54 @@ class Body extends StatelessWidget {
     return Column(
       children: <Widget>[
         Container(
-         height: size.height * 0.2 - 27,
-         decoration: BoxDecoration(
-          color: kPrimaryColor,
-          borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(36),
-            bottomRight: Radius.circular(36),
+          height: size.height * 0.2 - 27,
+          decoration: BoxDecoration(
+            color: kPrimaryColor,
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(36),
+              bottomRight: Radius.circular(36),
+            ),
           ),
-         ), 
-        )
+        ),
+        Positioned(
+          bottom: 0,
+          left: 0,
+          right: 0,
+          child: Container(
+            alignment: Alignment.center,
+            margin: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+            padding: EdgeInsets.symmetric(horizontal: kDefaultPadding),
+            height: 54,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                  offset: Offset(0, 10),
+                  blurRadius: 30,
+                  color: kPrimaryColor.withOpacity(0.23),
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    onChanged: (value) {},
+                    decoration: InputDecoration(
+                      hintText: 'Search',
+                      hintStyle: TextStyle(color: kTextColor.withOpacity(0.5)),
+                      enabledBorder: InputBorder.none,
+                      focusedBorder: InputBorder.none,
+                      // suffixIcon: SvgPicture.asset("assets/icons/search.svg"),
+                    ),
+                  ),
+                ),
+                SvgPicture.asset("assets/icons/search.svg"),
+              ],
+            ),
+          ),
+        ),
       ],
     );
   }
