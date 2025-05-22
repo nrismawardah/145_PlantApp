9e6c8d59beb18168aeded0d326e85a3139299df9
nrismawardah <nrismawardah@gmail.com>
2025-05-23 02:31:46 +0700

    Memperbarui tampilan untuk body pada details

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -17,6 +17,33 @@ class Body extends StatelessWidget {
         children: [
           ImagesAndIcons(size: size),
           TitleAndPrice(title: "Angelica", country: "Russia", price: 440),
+          SizedBox(height: kDefaultPadding),
+          Row(
+            children: <Widget>[
+              SizedBox(
+                width: size.width / 2,
+                height: 84,
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    backgroundColor: kPrimaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topRight: Radius.circular(20),
+                      ),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text(
+                    "Buy Now",
+                    style: TextStyle(color: Colors.white, fontSize: 16),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: TextButton(onPressed: () {}, child: Text("Description")),
+              ),
+            ],
+          ),
         ],
       ),
     );
