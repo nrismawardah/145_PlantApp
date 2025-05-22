8e9e65f6463e7926314825c7ea5da3882820ef2d
nrismawardah <nrismawardah@gmail.com>
2025-05-22 19:51:26 +0700

    Menambahkan pengaturan warna di main dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,7 @@
-const kPrimaryColor = 0xFF0C9869;
-const kTextColor = 0xFF3C4046;
-const kBackgroundColor = 0xFFF9F8FD;
+import 'package:flutter/cupertino.dart';
+
+const kPrimaryColor = Color(0xFF0C9869);
+const kTextColor = Color(0xFF3C4046);
+const kBackgroundColor = Color(0xFFF9F8FD);
 
 const double kDefaultPadding = 20.0;
