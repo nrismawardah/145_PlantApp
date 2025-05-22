9537aa40926fa7e3f27eee402d0927bedf62c001
nrismawardah <nrismawardah@gmail.com>
2025-05-22 20:10:52 +0700

    Membuat stateless widget untuk body page

---
+++ lib/screens/home/components/body.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
