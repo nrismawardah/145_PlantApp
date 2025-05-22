a27f5802fb95d10df150fc63466f9eeac41f115e
nrismawardah <nrismawardah@gmail.com>
2025-05-23 02:26:59 +0700

    Memperbarui tampilan untuk body pada details

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -12,7 +12,63 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
     return SingleChildScrollView(
-      child: Column(children: [ImagesAndIcons(size: size)]),
+      child: Column(
+        children: [
+          ImagesAndIcons(size: size),
+          TitleAndPrice(title: "Angelica", country: "Russia", price: 440),
+        ],
+      ),
+    );
+  }
+}
+
+class TitleAndPrice extends StatelessWidget {
+  const TitleAndPrice({
+    super.key,
+    required this.title,
+    required this.country,
+    required this.price,
+  });
+
+  final String title, country;
+  final int price;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+      child: Row(
+        children: <Widget>[
+          RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: "$title\n",
+                  style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                    color: kTextColor,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                TextSpan(
+                  text: country,
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: kPrimaryColor,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Spacer(),
+          Text(
+            "\$$price",
+            style: Theme.of(
+              context,
+            ).textTheme.headlineLarge?.copyWith(color: kPrimaryColor),
+          ),
+        ],
+      ),
     );
   }
 }
