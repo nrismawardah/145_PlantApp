5cb6cffda21ce1c80d6b6c40a0ba01f9716e856f
nrismawardah <nrismawardah@gmail.com>
2025-05-23 02:13:55 +0700

    Memperbarui tampilan untuk body pada details

---
+++ lib/screens/details/components/body.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:plantapp/constants.dart';
+
+class Body extends StatelessWidget {
+  const Body({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Column(
+      children: [
+        SizedBox(
+          height: size.height * 0.8,
+          child: Row(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: kDefaultPadding * 3,
+                  ),
+                  child: Column(
+                    children: [
+                      Align(
+                        alignment: Alignment.topLeft,
+                        child: IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: SvgPicture.asset("assets/icons/back_arrow.svg"),
+                          padding: EdgeInsets.symmetric(
+                            horizontal: kDefaultPadding,
+                          ),
+                        ),
+                      ),
+                      Spacer(),
+                      IconCard(icon: "assets/icons/sun.svg"),
+                      IconCard(icon: "assets/icons/icon_2.svg"),
+                      IconCard(icon: "assets/icons/icon_3.svg"),
+                      IconCard(icon: "assets/icons/icon_4.svg"),
+                    ],
+                  ),
+                ),
+              ),
+              Container(
+                height: size.height * 0.8,
+                width: size.width * 0.75,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(63),
+                    bottomLeft: Radius.circular(63),
+                  ),
+                  boxShadow: [
+                    BoxShadow(
+                      offset: Offset(0, 10),
+                      blurRadius: 60,
+                      color: kPrimaryColor.withOpacity(0.29),
+                    ),
+                  ],
+                  image: DecorationImage(
+                    alignment: Alignment.centerLeft,
+                    fit: BoxFit.cover,
+                    image: AssetImage("assets/images/img.png"),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class IconCard extends StatelessWidget {
+  const IconCard({super.key, required this.icon});
+
+  final String icon;
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: size.height * 0.03),
+      padding: EdgeInsets.all(kDefaultPadding / 2),
+      height: 62,
+      width: 62,
+      decoration: BoxDecoration(
+        color: kBackgroundColor,
+        borderRadius: BorderRadius.circular(6),
+        boxShadow: [
+          BoxShadow(
+            offset: Offset(0, 15),
+            blurRadius: 22,
+            color: kPrimaryColor.withOpacity(0.22),
+          ),
+          BoxShadow(
+            offset: Offset(-15, -15),
+            blurRadius: 20,
+            color: Colors.white,
+          ),
+        ],
+      ),
+      child: SvgPicture.asset(icon),
+    );
+  }
+}
