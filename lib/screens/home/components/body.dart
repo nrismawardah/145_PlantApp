06f8692106f81e096e4cd2618b166432725c7df6
nrismawardah <nrismawardah@gmail.com>
2025-05-22 20:18:24 +0700

    Membuat container pada body

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,10 +1,25 @@
 import 'package:flutter/cupertino.dart';
+import 'package:plantapp/constants.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Size size = MediaQuery.of(context).size;
+    return Column(
+      children: <Widget>[
+        Container(
+         height: size.height * 0.2 - 27,
+         decoration: BoxDecoration(
+          color: kPrimaryColor,
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(36),
+            bottomRight: Radius.circular(36),
+          ),
+         ), 
+        )
+      ],
+    );
   }
 }
