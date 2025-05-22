1884308ddcd56678ca1d4508541cb75209adfd63
nrismawardah <nrismawardah@gmail.com>
2025-05-23 01:38:48 +0700

    Memperbarui tampilan untuk body

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:plantapp/constants.dart';
 import 'package:plantapp/screens/home/components/header_with_searchbox.dart';
+import 'package:plantapp/screens/home/components/recommend_plants.dart';
 import 'package:plantapp/screens/home/components/title_with_more_btn.dart';
 
 class Body extends StatelessWidget {
@@ -17,14 +18,17 @@ class Body extends StatelessWidget {
           HeaderWithSearchBox(size: size),
           TitleWithMoreBtn(title: "Recomended", press: () {}),
           RecommendPlants(),
+          TitleWithMoreBtn(title: "Featured Plants", press: () {}),
+          FeaturedPlants(),
+          SizedBox(height: kDefaultPadding),
         ],
       ),
     );
   }
 }
 
-class RecommendPlants extends StatelessWidget {
-  const RecommendPlants({super.key});
+class FeaturedPlants extends StatelessWidget {
+  const FeaturedPlants({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -32,25 +36,12 @@ class RecommendPlants extends StatelessWidget {
       scrollDirection: Axis.horizontal,
       child: Row(
         children: [
-          RecomendPlantCard(
-            image: "assets/images/image_1.png",
-            title: "Samantha",
-            country: "Russia",
-            price: 440,
+          FeaturePlantCard(
+            image: "assets/images/bottom_img_1.png",
             press: () {},
           ),
-          RecomendPlantCard(
-            image: "assets/images/image_2.png",
-            title: "Angelica",
-            country: "Russia",
-            price: 440,
-            press: () {},
-          ),
-          RecomendPlantCard(
-            image: "assets/images/image_3.png",
-            title: "Samantha",
-            country: "Russia",
-            price: 440,
+          FeaturePlantCard(
+            image: "assets/images/bottom_img_2.png",
             press: () {},
           ),
         ],
@@ -59,81 +50,28 @@ class RecommendPlants extends StatelessWidget {
   }
 }
 
-class RecomendPlantCard extends StatelessWidget {
-  const RecomendPlantCard({
-    super.key,
-    required this.image,
-    required this.title,
-    required this.country,
-    required this.price,
-    required this.press,
-  });
-
-  final String image, title, country;
-  final int price;
+class FeaturePlantCard extends StatelessWidget {
+  const FeaturePlantCard({super.key, required this.image, required this.press});
+  final String image;
   final VoidCallback press;
 
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
-      margin: EdgeInsets.only(
-        left: kDefaultPadding,
-        top: kDefaultPadding / 2,
-        bottom: kDefaultPadding * 2.5,
-      ),
-      width: size.width * 0.4,
-      child: Column(
-        children: [
-          Image.asset(image),
-          GestureDetector(
-            onTap: press,
-            child: Container(
-              padding: EdgeInsets.all(kDefaultPadding / 2),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(10),
-                  bottomRight: Radius.circular(10),
-                ),
-                boxShadow: [
-                  BoxShadow(
-                    blurRadius: 50,
-                    offset: Offset(0, 10),
-                    color: kPrimaryColor.withOpacity(0.23),
-                  ),
-                ],
-              ),
-              child: Row(
-                children: [
-                  RichText(
-                    text: TextSpan(
-                      children: [
-                        TextSpan(
-                          text: "$title\n".toUpperCase(),
-                          style: Theme.of(context).textTheme.labelLarge,
-                        ),
-                        TextSpan(
-                          text: "$country".toUpperCase(),
-                          style: TextStyle(
-                            color: kPrimaryColor.withOpacity(0.5),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Spacer(),
-                  Text(
-                    "\$$price",
-                    style: Theme.of(
-                      context,
-                    ).textTheme.labelLarge?.copyWith(color: kPrimaryColor),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
+    return GestureDetector(
+      onTap: press,
+      child: Container(
+        margin: EdgeInsets.only(
+          left: kDefaultPadding,
+          top: kDefaultPadding / 2,
+          bottom: kDefaultPadding / 2,
+        ),
+        width: size.width * 0.8,
+        height: 185,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          image: DecorationImage(fit: BoxFit.cover, image: AssetImage(image)),
+        ),
       ),
     );
   }
