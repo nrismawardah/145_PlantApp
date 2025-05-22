3eed07bf44035b1034d5d4b6e40a4a471ea904a0
nrismawardah <nrismawardah@gmail.com>
2025-05-23 02:20:19 +0700

    Memperbarui tampilan untuk body pada details

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -10,66 +10,78 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Column(
-      children: [
-        SizedBox(
-          height: size.height * 0.8,
-          child: Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(
-                    vertical: kDefaultPadding * 3,
-                  ),
-                  child: Column(
-                    children: [
-                      Align(
-                        alignment: Alignment.topLeft,
-                        child: IconButton(
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          icon: SvgPicture.asset("assets/icons/back_arrow.svg"),
-                          padding: EdgeInsets.symmetric(
-                            horizontal: kDefaultPadding,
-                          ),
+    return SingleChildScrollView(
+      child: Column(children: [ImagesAndIcons(size: size)]),
+    );
+  }
+}
+
+class ImagesAndIcons extends StatelessWidget {
+  const ImagesAndIcons({super.key, required this.size});
+
+  final Size size;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: kDefaultPadding * 3),
+      child: SizedBox(
+        height: size.height * 0.8,
+        child: Row(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  vertical: kDefaultPadding * 3,
+                ),
+                child: Column(
+                  children: [
+                    Align(
+                      alignment: Alignment.topLeft,
+                      child: IconButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        icon: SvgPicture.asset("assets/icons/back_arrow.svg"),
+                        padding: EdgeInsets.symmetric(
+                          horizontal: kDefaultPadding,
                         ),
                       ),
-                      Spacer(),
-                      IconCard(icon: "assets/icons/sun.svg"),
-                      IconCard(icon: "assets/icons/icon_2.svg"),
-                      IconCard(icon: "assets/icons/icon_3.svg"),
-                      IconCard(icon: "assets/icons/icon_4.svg"),
-                    ],
-                  ),
-                ),
-              ),
-              Container(
-                height: size.height * 0.8,
-                width: size.width * 0.75,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(63),
-                    bottomLeft: Radius.circular(63),
-                  ),
-                  boxShadow: [
-                    BoxShadow(
-                      offset: Offset(0, 10),
-                      blurRadius: 60,
-                      color: kPrimaryColor.withOpacity(0.29),
                     ),
+                    Spacer(),
+                    IconCard(icon: "assets/icons/sun.svg"),
+                    IconCard(icon: "assets/icons/icon_2.svg"),
+                    IconCard(icon: "assets/icons/icon_3.svg"),
+                    IconCard(icon: "assets/icons/icon_4.svg"),
                   ],
-                  image: DecorationImage(
-                    alignment: Alignment.centerLeft,
-                    fit: BoxFit.cover,
-                    image: AssetImage("assets/images/img.png"),
-                  ),
                 ),
               ),
-            ],
-          ),
+            ),
+            Container(
+              height: size.height * 0.8,
+              width: size.width * 0.75,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(63),
+                  bottomLeft: Radius.circular(63),
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    offset: Offset(0, 10),
+                    blurRadius: 60,
+                    color: kPrimaryColor.withOpacity(0.29),
+                  ),
+                ],
+                image: DecorationImage(
+                  alignment: Alignment.centerLeft,
+                  fit: BoxFit.cover,
+                  image: AssetImage("assets/images/img.png"),
+                ),
+              ),
+            ),
+          ],
         ),
-      ],
+      ),
     );
   }
 }
