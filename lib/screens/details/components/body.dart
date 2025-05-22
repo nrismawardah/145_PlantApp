719219b2c6554c7cf58d66bbc203b34f13b7cc4f
nrismawardah <nrismawardah@gmail.com>
2025-05-23 02:28:32 +0700

    Memindahkan class TitleAndPrice ke file yg berbeda

--- lib/screens/details/components/body.dart
+++ lib/screens/details/components/body.dart
@@ -4,6 +4,7 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:plantapp/constants.dart';
 import 'package:plantapp/screens/details/components/icon_card.dart';
 import 'package:plantapp/screens/details/components/images_and_icons.dart';
+import 'package:plantapp/screens/details/components/title_and_price.dart';
 
 class Body extends StatelessWidget {
   const Body({super.key});
@@ -16,57 +17,6 @@ class Body extends StatelessWidget {
         children: [
           ImagesAndIcons(size: size),
           TitleAndPrice(title: "Angelica", country: "Russia", price: 440),
-        ],
-      ),
-    );
-  }
-}
-
-class TitleAndPrice extends StatelessWidget {
-  const TitleAndPrice({
-    super.key,
-    required this.title,
-    required this.country,
-    required this.price,
-  });
-
-  final String title, country;
-  final int price;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
-      child: Row(
-        children: <Widget>[
-          RichText(
-            text: TextSpan(
-              children: [
-                TextSpan(
-                  text: "$title\n",
-                  style: Theme.of(context).textTheme.headlineMedium?.copyWith(
-                    color: kTextColor,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                TextSpan(
-                  text: country,
-                  style: TextStyle(
-                    fontSize: 20,
-                    color: kPrimaryColor,
-                    fontWeight: FontWeight.w300,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Spacer(),
-          Text(
-            "\$$price",
-            style: Theme.of(
-              context,
-            ).textTheme.headlineLarge?.copyWith(color: kPrimaryColor),
-          ),
         ],
       ),
     );
