acf27611be4543c1a7fae08f6656cc85e9d588c9
nrismawardah <nrismawardah@gmail.com>
2025-05-23 02:00:18 +0700

    Membuat tampilan untuk details screen

--- lib/screens/details/details_screen.dart
+++ lib/screens/details/details_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:plantapp/screens/details/components/body.dart';
 
 class DetailsScreen extends StatelessWidget {
   const DetailsScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Body(),
+    );
   }
 }
